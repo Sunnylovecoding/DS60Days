4620bc04e8d4bc8a16f4c864bf06e1edd8580dfa
Sunnylovecoding <58130998+Sunnylovecoding@users.noreply.github.com>
2021-03-17 22:20:28 -0800

    Update homework_12.py

--- homework_12.py
+++ homework_12.py
@@ -4,6 +4,8 @@ data = pd.read_csv('boston.csv')
 #畫出箱型圖，並判斷哪個欄位的中位數在 300~400 之間？
 print(type(data))
 print(data.head())
-
 data.boxplot()
+#ANS:可知tax欄位的中位數落在300~400之間
 #畫出散佈圖 x='NOX', y='DIS' ，並說明這兩欄位有什麼關係？
+data.plot.scatter(x='NOX', y='DIS')
+#ANS:NOX與DIS呈現負相關,隨NOX值增加,DIS值減少
